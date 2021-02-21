23ecc23467fb7ca463672c5e1f207a455959349c
benj <bmp22@protonmail.com>
2021-02-21 23:46:17 +0200

    fix 3

--- src/component/Links.jsx
+++ src/component/Links.jsx
@@ -5,19 +5,21 @@ import './links.scss';
 import { ReactComponent as LinkedIn } from '../image/linkedin.svg';
 import { ReactComponent as GitHub } from '../image/github.svg';
 
-const Links = () => (
-  <div className="links">
-    <div className="links__link">
-      <a href="https://www.linkedin.com/in/banjopower/" target="_blank" rel="noreferrer">
-        <LinkedIn/>
-      </a>
+function Links() {
+  return (
+    <div className="links">
+      <div className="links__link">
+        <a href="https://www.linkedin.com/in/banjopower/" target="_blank" rel="noreferrer">
+          <LinkedIn/>
+        </a>
+      </div>
+      <div className="links__link">
+        <a href="https://github.com/benj-power" target="_blank" rel="noreferrer">
+          <GitHub/>
+        </a>
+      </div>
     </div>
-    <div className="links__link">
-      <a href="https://github.com/benj-power" target="_blank" rel="noreferrer">
-        <GitHub/>
-      </a>
-    </div>
-  </div>
-);
+  );
+}
 
 export default Links;
