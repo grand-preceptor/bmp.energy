1790bd80d01e53ca9cd4d863b3a85c49000498bc
benj <bmp22@protonmail.com>
2021-02-21 12:05:17 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GET OFF MY LAWN</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
