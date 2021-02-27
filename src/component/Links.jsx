f2e20d21f7d8fed11ae7d31bacabe3ba55c32c68
benj <bmp22@protonmail.com>
2021-02-27 21:18:55 +0200

    fix

--- src/component/Links.jsx
+++ src/component/Links.jsx
@@ -5,21 +5,19 @@ import './links.scss';
 import { ReactComponent as LinkedIn } from '../image/linkedin.svg';
 import { ReactComponent as GitHub } from '../image/github.svg';
 
-function Links() {
-  return (
-    <div className="links">
-      <div className="links__link">
-        <a href="https://www.linkedin.com/in/banjopower/" target="_blank" rel="noreferrer">
-          <LinkedIn/>
-        </a>
-      </div>
-      <div className="links__link">
-        <a href="https://github.com/benj-power" target="_blank" rel="noreferrer">
-          <GitHub/>
-        </a>
-      </div>
+const Links = () => (
+  <div className="links">
+    <div className="links__link">
+      <a href="https://www.linkedin.com/in/banjopower/" target="_blank" rel="noreferrer">
+        <LinkedIn/>
+      </a>
     </div>
-  );
-}
+    <div className="links__link">
+      <a href="https://github.com/benj-power" target="_blank" rel="noreferrer">
+        <GitHub/>
+      </a>
+    </div>
+  </div>
+);
 
 export default Links;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -5,22 +5,20 @@ import Links from '../component/Links';
 import './home.scss';
 import '../stylesheet/_common.scss';
 
-function Home() {
-  return (
-    <div className="d-flex">
-      <div className="w-50">
-        <h1>Benjamin Power</h1>
-        <p>
-          Been a member of numerous teams so able to communicate with juniors, seniors, business
-          analysts,
-          managers, and executives. Forte is building enterprise web applications. Written many RESTful
-          APIs and responsive Angular/React UIs. Love writing, reviewing, and discussing code. I attempt
-          to study everything in life; there is much that fascinates me. Reader, questioner, ruminator.
-        </p>
-        <Links/>
-      </div>
+const Home = () => (
+  <div className="d-flex">
+    <div className="w-50">
+      <h1>Benjamin Power</h1>
+      <p>
+        Been a member of numerous teams so able to communicate with juniors, seniors, business
+        analysts,
+        managers, and executives. Forte is building enterprise web applications. Written many RESTful
+        APIs and responsive Angular/React UIs. Love writing, reviewing, and discussing code. I attempt
+        to study everything in life; there is much that fascinates me. Reader, questioner, ruminator.
+      </p>
+      <Links/>
     </div>
-  );
-}
+  </div>
+);
 
 export default Home;
